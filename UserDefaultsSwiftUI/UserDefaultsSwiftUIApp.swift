d7cc8cd445bc85cf1cd1612efde53af24a179cf1
Alex Nagy <therebeloper@gmail.com>
2020-10-13 13:11:47 +0300

    Initial Commit

---
+++ UserDefaultsSwiftUI/UserDefaultsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsSwiftUIApp.swift
+//  UserDefaultsSwiftUI
+//
+//  Created by Alex Nagy on 13/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
