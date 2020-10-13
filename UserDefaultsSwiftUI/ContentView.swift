04e718f9e0c23682b3c324b871d3f0f71aa24f84
Alex Nagy <therebeloper@gmail.com>
2020-10-13 13:26:56 +0300

    first commit

--- UserDefaultsSwiftUI/ContentView.swift
+++ UserDefaultsSwiftUI/ContentView.swift
@@ -7,10 +7,29 @@
 
 import SwiftUI
 
+class ContentViewModel: ObservableObject {
+    @Published("profile") var profile = Profile(name: "Alex", age: 10)
+}
+
 struct ContentView: View {
+    
+    @AppStorage("score") var score = 0
+    
+    @AppStorage("cool", store: UserDefaults(suiteName: "group.com.rebeloper.UserDefaultsSwiftUI")) var isCool = false
+    
+    @StateObject var model = ContentViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, \(model.profile.name)!")
+            .padding().onTapGesture(count: 1, perform: {
+                score += 1
+                print(score)
+                
+                isCool.toggle()
+                print(isCool)
+                
+                model.profile.name = "Alexander"
+            })
     }
 }
 
